16869a187890316d50a74ccdd1f459cb01c9fa89
Gabriel Castro <gabriel.castromunoz@aalto.fi>
2020-04-13 14:35:28 +0300

    Start the project
    Created Midi connections and logging messages received

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var midi = null;  // global MIDIAccess object
+
+    function onMIDISuccess( midiAccess ) {
+      console.log( "MIDI ready!" );
+      console.log(midiAccess)
+      // midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+      listInputsAndOutputs( midiAccess );
+      startLoggingMIDIInput( midiAccess );
+    }
+
+    function onMIDIFailure(msg) {
+      console.log( "Failed to get MIDI access - " + msg );
+    }
+
+    function listInputsAndOutputs( midiAccess ) {
+      console.log(midiAccess.inputs.size)
+      console.log("MIDI INPUTS")
+      midiAccess.inputs.forEach(function(input) {
+        console.log( input );
+      });
+      console.log("MIDI OUTPUTS")
+      midiAccess.outputs.forEach(function(output) {
+        console.log( output);
+      });
+    }
+    navigator.requestMIDIAccess({ sysex: true }).then( onMIDISuccess, onMIDIFailure );
+    function onMIDIMessage( event ) {
+      var str = "MIDI message received at timestamp " + event.timestamp + "[" + event.data.length + " bytes]: ";
+      for (var i=0; i<event.data.length; i++) {
+        str += event.data[i].toString(8) + " ";
+      }
+      console.log( str );
+      console.log(event)
+    }
+    
+    function startLoggingMIDIInput( midiAccess ) {
+      midiAccess.inputs.forEach( function(entry) {entry.onmidimessage = onMIDIMessage;});
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    v
     </div>
   );
 }
