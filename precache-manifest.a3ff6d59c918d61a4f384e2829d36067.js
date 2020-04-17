38645eb266a60f66bee4d929cc76b824e0b58749
Gabriel Castro <gabriel.castromunoz@aalto.ficd>
2020-04-17 16:06:05 +0300

    Updates

---
+++ precache-manifest.a3ff6d59c918d61a4f384e2829d36067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49b0faa49f8545eaa87cc21874120cf",
+    "url": "/visuals/index.html"
+  },
+  {
+    "revision": "5092dddba73659bf98a8",
+    "url": "/visuals/static/css/main.de1db213.chunk.css"
+  },
+  {
+    "revision": "89e64e78aadb93c8eef3",
+    "url": "/visuals/static/js/2.5a854e88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/visuals/static/js/2.5a854e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5092dddba73659bf98a8",
+    "url": "/visuals/static/js/main.07e0c920.chunk.js"
+  },
+  {
+    "revision": "267e884750934c00e6e7",
+    "url": "/visuals/static/js/runtime-main.ffd3cab0.js"
+  }
+]);
